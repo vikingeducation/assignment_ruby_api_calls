d75ddc9817eecda21cd4951169e89c9fb9d48b76
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-07-13 18:34:11 -0500

    add_commit

--- github_activity_monitor.rb
+++ github_activity_monitor.rb
@@ -13,8 +13,21 @@ class GithubMonitor
     @git.repos.create(name: "forkcommithistory")
   end
 
-  def add_commit
-    contents = Github::Client::Repos::Contents.new
-    contents.create( ENV['USERNAME'], g.repo.name, "README.md", path: "README.md", message: "create readme file", content: "testing")
+  def add_readme
+    @contents = Github::Client::Repos::Contents.new(oauth_token: ENV["TOKEN"])
+    @contents.create( ENV['USERNAME'], @repo, "README.md", path: "README.md", message: "create readme file", content: "testing")
   end
+
+  def get_commit(repo)
+    # Git::Repos::Commits(user, repo)
+    commit = @git.repos.commits.list(ENV["USERNAME"], repo).keys
+  end
+
+  def add_last_commit(repo)
+    # commits[0]["commit"]["author"]["date"]
+    msg = @git.repos.commits.list(ENV["USERNAME"], repo)[0]["commit"]["author"]["date"]
+    file = @git.repos.contents.find(user: ENV["USERNAME"], repo: @repo, path: "README.md")
+    @git.repos.contents.update( ENV['USERNAME'], @repo, "README.md", path: "README.md", message: msg, content: "testing", sha: file.sha)
+  end
+
 end
