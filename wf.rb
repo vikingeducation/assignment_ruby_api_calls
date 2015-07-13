a278b266ac5f7960e99ab9831516846792202c72
facingsouth <xin.d.liu@gmail.com>
2015-07-13 15:15:03 -0700

    finish

--- wf.rb
+++ wf.rb
@@ -1,14 +1,9 @@
 require 'httparty'
 require 'json'
-
 require 'pry-byebug'
 require 'pp'
 
-
-
 class WeatherForecast
-
-  # BASE_URI = "http://api.openweathermap.org/data/2.5/"
 
   API_KEY = ENV["API_KEY"]
 
@@ -19,21 +14,78 @@ class WeatherForecast
 
   include HTTParty
 
-  base_uri('http://api.openweather.org/data/2.5/')
+  base_uri('api.openweathermap.org')
 
   def initialize(city_name, num_of_days)
-    @options = { :query => { :q => city_name, :mode => "json", :cnt => num_of_days, :APPID => API_KEY }}
+    @options = { :query => { :q => city_name, :mode => "JSON", :cnt => num_of_days, :units => "imperial", "api-key" => API_KEY  }}
 
     # params = { "api-key" => API_KEY }
   end
 
-
   def current
-    self.class.get("weather?", @options)
+    self.class.get("/data/2.5/weather?", @options)
   end
 
   def forecast
-    self.class.get("forecast/daily?", @options)
+    self.class.get("/data/2.5/forecast/daily?", @options)
+  end
+
+  def hi_temps
+    response = forecast
+    response_body = JSON.parse(response.body)
+    response_body["list"].map {|l| l["temp"]["max"]}
+  end
+
+  def low_temps
+    response = forecast
+    response_body = JSON.parse(response.body)
+    response_body["list"].map {|l| l["temp"]["min"]}
+  end
+
+  def weather
+    response = forecast
+    response_body = JSON.parse(response.body)
+    response_body["list"].map {|l| l["weather"][0]["main"]}
+  end
+
+  def description
+    response = forecast
+    response_body = JSON.parse(response.body)
+    response_body["list"].map {|l| l["weather"][0]["description"]}
+  end
+
+  def wind
+    response = forecast
+    response_body = JSON.parse(response.body)
+    response_body["list"].map {|l| l["speed"]}
+  end
+
+  def today
+    response = current
+    response_body = JSON.parse(response.body)
+    hi_temps = response_body["main"]["temp_max"]
+    low_temps = response_body["main"]["temp_min"]
+    weather = response_body["weather"][0]["main"]
+    des = response_body["weather"][0]["description"]
+    wind = response_body["wind"]
+    puts "Today, #{weather}, #{des}."
+    puts "Tempurature from #{hi_temps} to #{low_temps}."
+    puts "Wind #{wind}."
+  end
+
+  def tomorrow
+    response = forecast
+    response_body = JSON.parse(response.body)
+    l =response_body["list"][0]
+    hi_temps = l["temp"]["max"]
+    low_temps = l["temp"]["min"]
+    weather = l["weather"][0]["main"]
+    des = l["weather"][0]["description"]
+    wind = l["speed"]
+
+    puts "Tomorrow, #{weather}, #{des}."
+    puts "Tempurature from #{hi_temps} to #{low_temps}."
+    puts "Wind #{wind}."
   end
 
   # def send_request
@@ -80,19 +132,19 @@ class WeatherForecast
   # end
 
 
-  def send_request
-    # @options = { :query => { :zip => zipcode, :cnt=> num} }
+  # def send_request
+  #   # @options = { :query => { :zip => zipcode, :cnt=> num} }
     
-    # uri = [BASE_URI, @forecast_type, zipcode_uri, "&cnt=#{@num_of_days}"].join
-    base_uri ''
-    # puts uri.inspect
+  #   # uri = [BASE_URI, @forecast_type, zipcode_uri, "&cnt=#{@num_of_days}"].join
+  #   base_uri ''
+  #   # puts uri.inspect
 
-    params = { "api-key" => API_KEY }
+  #   params = { "api-key" => API_KEY }
 
-    response = HTTParty.get(uri, params)
-    # pp request.body
+  #   response = HTTParty.get(uri, params)
+  #   # pp request.body
 
-  end
+  # end
 
   # def validate_zip!(zipcode)
   #   raise "Invalid zip code." unless (10000..99999).include?(zipcode)
