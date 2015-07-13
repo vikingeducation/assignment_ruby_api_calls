f475331fc3d4fd316a87325e21d94de63cdea87c
Alice Fung <alice.o.fung@gmail.com>
2015-07-13 18:27:32 -0400

    create repo

--- github_activity_monitor.rb
+++ github_activity_monitor.rb
@@ -2,8 +2,10 @@ require "github_api"
 
 class GithubMonitor
 
+  attr_accessor :git, :repo
+
   def initialize
-    @git = Github.new(oauth_token: ENV["TOKEN"])
+    @git = Github.new(oauth_token: ENV["TOKEN"]) #obj
     @repo = create_repo
   end
 
@@ -12,6 +14,7 @@ class GithubMonitor
   end
 
   def add_commit
-    
+    contents = Github::Client::Repos::Contents.new
+    @git.repos.contents.create( ENV['USERNAME'], @repo, 'README.md')
   end
 end
