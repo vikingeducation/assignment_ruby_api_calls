542e2aaf6cabf17fe1f2bb3c730205309166b537
Alice Fung <alice.o.fung@gmail.com>
2015-07-13 16:24:04 -0400

    repo list fixed w/ order

--- github.rb
+++ github.rb
@@ -1,6 +1,6 @@
 require "github_api"
 
 github_obj = Github.new(oauth_token: ENV["GITHUB_VCS_API"])
-repo_list = github_obj.repos.list(user: ENV["USERNAME"], sort: "created")
+repo_list = github_obj.repos.list(user: ENV["USERNAME"], sort: "created", direction: "desc")
 
 
