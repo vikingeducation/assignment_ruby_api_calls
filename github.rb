521ad8a48ab58b034fd2f4b4d3d104a0b5923e21
Donald Kelsey <dmkelseyii@gmail.com>
2015-07-13 13:37:23 -0500

    Can pull last 10 repos updated.

--- github.rb
+++ github.rb
@@ -1,9 +1,9 @@
 
 require 'github_api'
-
-
-  
-
 github = Github.new oauth_token: File.readlines('key.md')[1]
 
-puts github.repos.list
+# github.repos.list(user: 'shadefinale').each{|r| puts r["updated_at"]}
+repos = github.repos.list(user: 'shadefinale', sort: 'created').map{|r| r[:name]}
+p repos[0..9]
+# Date.parse("2015-06-29T16:54:21Z")
+#github.repos.list {|repo| p repo["updated_at"]}
