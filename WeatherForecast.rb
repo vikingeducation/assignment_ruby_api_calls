0c205c4231dd4f24ddfe9ed6e10c6dd1d80f11d3
David <davidmeza@outlook.com>
2015-07-13 13:47:03 -0400

    forgot to save

--- WeatherForecast.rb
+++ WeatherForecast.rb
@@ -4,27 +4,40 @@ require "pp"
 
 class WeatherForecast
 
+  attr_accessor :weather_info
+
   include HTTParty
 
   base_uri('api.openweathermap.org')
 
   def initialize(location = "New_York,US", num_days = "3")
 
-    @options = { :query => { :q => location, :cnt => num_days, :mode => "JSON", :units => "imperial" } }
+    @options = { :query => { :q => location, :cnt => num_days, :mode => "JSON", :units => "imperial", APPID => ENV[API_KEY] } }
 
   end
 
   def current
 
-    current = self.class.get('/data/2.5/weather?', @options)
+    self.class.get('/data/2.5/weather?', @options)
     binding.pry
 
   end
 
   def forecast
 
-    forecast = self.class.get('/data/2.5/forecast?', @options)
-    binding.pry
+    self.class.get('/data/2.5/forecast?', @options)
+
+  end
+
+  def parse(response)
+
+    @weather_info = response.to_hash
+
+  end
+
+  def hi_temps
+
+
 
   end
 
