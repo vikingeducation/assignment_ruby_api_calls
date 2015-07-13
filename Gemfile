3e958ee1a4057a38960bd5bd6c549a25e31b60ee
Alice Fung <alice.o.fung@gmail.com>
2015-07-13 13:56:46 -0400

    finish weather forecast

--- Gemfile
+++ Gemfile
@@ -2,4 +2,3 @@ source "https://rubygems.org"
 
 gem "httparty"
 gem "pry"
-gem "pp"
--- weather_forecast.rb
+++ weather_forecast.rb
@@ -1,24 +1,86 @@
+require 'HTTParty'
+
 class WeatherForecast
 
   API_KEY = ENV["TOKEN"]
-
-  BASE_URI = "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID="
-
-  URI = BASE_URI + API_KEY
 
   def initialize(location = "Austin", days = 5)
 
     url = create_url(location, days)
 
     response = HTTParty.get(url)
+    @body = response.parsed_response
 
   end
 
 
   def create_url(location, days)
 
-    "api.openweathermap.org/data/2.5/forecast?q=#{location},us&mode=json&cnt=#{days}&APPID=#{API_KEY}"
+    "http://api.openweathermap.org/data/2.5/forecast?q=#{location},us&mode=json&cnt=#{days}&APPID=#{API_KEY}"
 
   end
+
+  def hi_temps
+    hi_temps = []
+    @body["list"].each do |day|
+      hi_temps << k_to_f(day["main"]["temp_max"])
+    end
+    hi_temps
+  end
+
+  def lo_temps
+    lo_temps = []
+    @body["list"].each do |day|
+      lo_temps << k_to_f(day["main"]["temp_min"])
+    end
+    lo_temps
+  end
+
+  def today
+    weather_details(0)
+  end
+
+  def tomorrow
+    weather_details(1)
+  end
+
+  def description
+    desc =[]
+    @body["list"].each do |day|
+      desc << day["weather"][0]["description"]
+    end
+    desc
+  end
+
+  def humidity
+    desc =[]
+    @body["list"].each do |day|
+      desc << day["main"]["humidity"]
+    end
+    desc
+  end
+
+  def avg_temp
+    t = []
+    @body["list"].each do |day|
+      t << k_to_f(day["main"]["temp"])
+    end
+    t.inject(:+)/t.length.to_f.round
+  end
+
+  def weather_details(day)
+    h = Hash.new
+    t = @body["list"][day]
+    h["max_temp"]= k_to_f(t["main"]["temp_max"])
+    h["min_temp"]= k_to_f(t["main"]["temp_min"])
+    h["humidity"]= t["main"]["humidity"]
+    h["description"] = t["weather"][0]["description"]
+    h
+  end
+
+  def k_to_f(temp_k)
+    ((temp_k-273.15) * 1.8 + 32).round
+  end
+
 
 end
