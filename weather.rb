87e12974498d62ac096fd7fb3e8e55c729f8b3ea
ayva <olga.ayva@gmail.com>
2015-07-13 10:13:57 -0700

    error fixed and new key passed

--- weather.rb
+++ weather.rb
@@ -5,7 +5,7 @@ class  WeatherForecast
   @@key=File.readlines('key.md')[0]
 
   def initialize (location="London", days=5)
-    @results=HTTParty.get("api.openweathermap.org/data/2.5/forecast?q=#{location}&cnt=#{days}&APPID=96a0ba87471a4d76285f7bfa76e9bbbb")
+    @results=HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=#{location}&cnt=#{days}&APPID=@@key")
     print
     #http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY}
   end
@@ -14,7 +14,11 @@ class  WeatherForecast
     pp @results
   end
 
+  def hi_temps
+
+  end
+
 
 end
-
 WeatherForecast.new
+puts @@key
