8cbcab32018def573843014db03e875d36c56c69
shadefinale <shadefinale@gmail.com>
2015-07-13 19:21:34 -0500

    Working date spoofer.

--- gh_mirror.rb
+++ gh_mirror.rb
@@ -1,5 +1,5 @@
 require 'github_api'
-require 'pry'
+require 'time'
 github = Github.new oauth_token: File.readlines('key.md')[1]
 token = File.readlines('key.md')[1]
 name = File.readlines('key.md')[2]
@@ -32,27 +32,38 @@ options = {
 # end
 
 commits = github.repos.commits.list(name, 'forkcommithistory')
-first_commit = commits[0]
-p first_commit['commit']['author']['date']
+target_dates = commits.map {|c| c['commit']['author']['date']}
+
+
+script = ""
+target_dates.each do |target_date|
+  new_date = (Time.parse(target_date) - 8000000).utc.iso8601.to_s
+
+  script += "echo #{new_date}>> README.md\ngit add .\ngit commit --date=\"#{new_date}\" -m=\"Spoof\"\n"
+end
+file = File.open("script.sh", "w")
+file.write(script)
+file.close
+
 
 # binding.pry
 
-contents = Github::Client::Repos::Contents.new oauth_token: File.readlines('key.md')[1]
+# contents = Github::Client::Repos::Contents.new oauth_token: File.readlines('key.md')[1]
 
-file = contents.find user: name, repo: "forkcommithistory", path: 'README.md'
+# file = contents.find user: name, repo: "forkcommithistory", path: 'README.md'
 
-#github.repos.list(name, "forkcommithistory")
+# #github.repos.list(name, "forkcommithistory")
 
-#opts = {"tree" => file.sha, "author" => {"name" => name, "email" => "shadefinale@gmail.com", "date" => "2015-04-04"}}
+# #opts = {"tree" => file.sha, "author" => {"name" => name, "email" => "shadefinale@gmail.com", "date" => "2015-04-04"}}
 
-contents.update name, "forkcommithistory", 'README.md',
-path: 'README.md',
-message: first_commit['commit']['message'],
-content: 'Olga & Donald assignment test',
-sha: file.sha,
-author: {name: name, email: "shadefinale@gmail.com", date: "2015-04-04"},
-commit: {committer: {name: name, email: "shadefinale@gmail.com", date: "2015-04-04"}},
-date: "2011-04-14T16:00:49Z"
+# contents.update name, "forkcommithistory", 'README.md',
+# path: 'README.md',
+# message: first_commit['commit']['message'],
+# content: 'Olga & Donald assignment test',
+# sha: file.sha,
+# author: {name: name, email: "shadefinale@gmail.com", date: "2011-04-14T16:00:49Z"},
+# commit: {committer: {name: name, email: "shadefinale@gmail.com", date: "2011-04-14T16:00:49Z"}},
+# date: "2011-04-14T16:00:49Z"
 
 # # github.repos.commits.list(user: name, repo: "forkcommithistory")
 
