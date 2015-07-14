681a54f9d21cd1cc665c0b88d416615b5e040875
facingsouth <xin.d.liu@gmail.com>
2015-07-13 18:00:20 -0700

    create new repo use github gem

--- github.rb
+++ github.rb
@@ -39,3 +39,46 @@ class GitThing
   end
 
 end
+
+
+# contents = Github::Client::Repos::Contents.new oauth_token:TOKEN
+# create new file in the repo
+# contents.create 'facingsouth', 'forkcommithistory', 'forkcommithistory/README.md', path: 'forkcommithistory/README.md', message: 'commit from github gem', content: "# forkcommithistory\nThis is my fork commmit history!\nsomething new!!"
+
+# update file in a repo
+# find the file
+# file = contents.find 'facingsouth', 'forkcommithistory', 'README.md'
+# update it
+# contents.create 'facingsouth', 'forkcommithistory', 'README.md', 
+                # path: 'forkcommithistory/README.md', 
+                # message: 'commit from github gem, change README', 
+                # content: "# forkcommithistory\nThis is my fork commmit history!\nsomething new!!", 
+                # sha: file.sha
+
+
+
+class MakeGitRepo
+
+  def initialize
+    @github = Github.new oauth_token: "396a1c70604556a4bbdad7e226abe00be65a4c8e"
+  end
+
+  def create_new_repo(repo_name = "forkcommithistory")
+    @github.repos.create name: "#{repo_name}"
+  end
+
+  def clone_git_repo(repo_name = "forkcommithistory")
+    repo = @github.repos.get 'facingsouth', repo_name
+    `git clone #{repo.html_url} ~/Desktop/VCS/#{repo_name}`
+  end
+
+end
+
+m = MakeGitRepo.new
+m.clone_git_repo
+
+
+
+
+
+
