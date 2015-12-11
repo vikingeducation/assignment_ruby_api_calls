f9deed72736e5ee85a475d505a0a8718a8f06adb
Sia <siakaramalegos@gmail.com>
2015-12-11 17:07:56 -0600

    Adds hi temps method

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -3,28 +3,41 @@ require 'awesome_print'
 require 'json'
 require 'pry'
 require 'envyable'
+require 'stamp'
 
 Envyable.load('env.yml')
 
 class WeatherForecast
-  BASE_URI = 'http://api.openweathermap.org/data/2.5/forecast?q='
+  BASE_URI = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='
   API_KEY = ENV['OPEN_WEATHER_API_KEY']
 
   def initialize(location = 'New Orleans', days = 5)
-    location.gsub!(' ', '%20')
+    @days = days
+    @location = location
     @forecast = send_request(location, days)
+  end
+
+  def hi_temps
+    puts "High Temperatures for the next #{@days} days in #{@location} (Fahrenheit):"
+    @forecast.each do |day|
+      date = Time.at(day['dt']).stamp("Mon, Dec 3")
+      high_temp = day['temp']['max']
+      puts " - #{date}: #{high_temp}"
+    end
   end
 
   private
 
   def send_request(location, days)
-    uri = "#{BASE_URI}#{location},USA&cnt=#{days}&APPID=#{API_KEY}"
+    uri_loc = location.gsub(' ', '%20')
+
+    uri = "#{BASE_URI}#{uri_loc},USA&cnt=#{days}&units=imperial&APPID=#{API_KEY}"
 
     response = HTTParty.get(uri)
-    binding.pry
 
     response["list"]
   end
 end
 
 forecast = WeatherForecast.new
+forecast.hi_temps
