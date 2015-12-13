3e82730193b6e28e40dc137a5ef3e9a9d38664f7
Sia <siakaramalegos@gmail.com>
2015-12-13 16:30:02 -0600

    Shows latest 10 commit messages for each of the latest 10 repos

--- github_app/github.rb
+++ github_app/github.rb
@@ -14,14 +14,25 @@ class GitHubRepos
   end
 
   # Lists latest 10 repos
-  def repos
-    repos = @github.repos.list(user: 'siakaramalegos', sort: 'updated', direction: 'desc', page: 1, per_page: 10).body
+  def repos(show_commits = false)
+    response = @github.repos.list(user: 'siakaramalegos', sort: 'updated', direction: 'desc', page: 1, per_page: 10)
+    repos = response.body
 
     repos.each_with_index do |repo, index|
       puts '-' * 80
       date_string = repo.updated_at
       date = DateTime.parse(date_string).to_date
       puts "(#{index + 1}) #{repo.name}: #{repo.description} (updated: #{date.stamp('12/30/15')})"
+
+      if show_commits
+        repo_commits = @github.repos.commits.list('siakaramalegos', repo.name, page: 1, per_page: 10).body
+
+        repo_commits.each do |c|
+          date_string = c.commit.author.date
+          date = DateTime.parse(date_string).to_date
+          puts "    #{c.commit.message} (#{date.stamp('12/30/15')})"
+        end
+      end
     end
     puts '-' * 80
     repos
@@ -30,4 +41,4 @@ end
 
 g = GitHubRepos.new
 # binding.pry
-g.repos
+g.repos(true)
