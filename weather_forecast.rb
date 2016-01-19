96ff877777f5f02b435be471ea80ddda4eecc669
Andrew Baik <and.baik@gmail.com>
2016-01-19 13:05:52 -0500

    added get_base_uri and trim_response methods

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -6,31 +6,35 @@ class WeatherForecast
   API_KEY = ENV["OPEN_WEATHER_MAP_KEY"]
   JSON_FORMAT = "mode=json"
 
-  def initialize(location="524901",days=5, daily=true)
-     
+  def initialize(location="London,us",days=5, daily=true)
+
     raise ArgumentError if !days.is_a?(Integer) && days > 0
-    raise ArgumentError if !location.is_a?(Fixnum) || !location.is_a?(String)
+    raise ArgumentError if !location.is_a?(Fixnum) && !location.is_a?(String)
 
     #validate_time_period!(time_period)
     #validate_format!(response_format)
     @location = location
     @days = days
-    @daily_str = daily ? "/daily" : ""  
+    @daily_str = daily ? "/daily" : ""
     @location = location.is_a?(String) ? "q=#{location}" : "id=#{location}"
     @response = nil
+    @api_str = "APPID=#{API_KEY}"
   end
 
   def get_base_uri
-    @response = HTTParty.get(BASE_URI + "#{@daily_str}?id=#{@location}&#{JSON_FORMAT}")
-  end  
+    uri = BASE_URI + "#{@daily_str}&#{@location}&#{JSON_FORMAT}&#{@api_str}"
+    @response = HTTParty.get(uri)
+  end
 
-
-
+  def trim_response
+    response_body = JSON.parse(@response.body)
+  end
 
 end
 
 w_api = WeatherForecast.new("London,us",3,false)
-
+w_api.get_base_uri
+p w_api.trim_response
 
 
 # # Actually run the request using their `get` convenience method
