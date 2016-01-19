f14db44c839dceeeb48cfb94ba7833065eea80a4
Thomas Lo <thomasjinlo@gmail.com>
2016-01-19 11:33:01 -0800

    FIgure out to read commits

--- Github/github_api.rb
+++ Github/github_api.rb
@@ -12,15 +12,26 @@ Figaro.load
 class GithubAPI
   TOKEN = Figaro.env.GITHUB_API
 
-  attr_accessor :client
+  attr_accessor :client, :repos
 
   def initialize
     @client = Github.new
     @client.current_options[:oauth_token] = TOKEN
   end
 
+  def get_repos(num)
+    @repos = @client.repos.list[-num..-1]
+  end
+
+  def get_commits
+    @repos[0].commits.get
+  end
+
 end
 
 git = GithubAPI.new
-binding.pry
-pp git.client.repos.list[0]
+# binding.pry
+# pp git.client.repos.list[0]
+git.get_repos(3)
+pp git.get_commits
+# pp git.repos
