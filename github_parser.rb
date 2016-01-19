aa03fd62cb166e40302c5fdacc3ae8efbf9bd8ad
Deepa Kackar <dkackar99@yahoo.com>
2016-01-19 15:48:36 -0800

    Added recent_commits proc

--- github_parser.rb
+++ github_parser.rb
@@ -14,15 +14,26 @@ class GithubParser
   def list_repos(user=nil)
     @user = user
     repos = @github.repos.list(sort: "updated", user: @user).first(10)
-    list = repos.map { |repo| repo["name"] }
+    #pp repos[0]
+    list = repos.map { |repo| [repo["owner"]["login"],repo["name"]] }
   end
 
   def list_commits
     recent_commits = []
-    list_repos.each do |repo_name|
+    
+    list_repos.each do |user_repo|
       sleep(1)
-      recent_commits << ["#{repo_name}", @github.repos.commits.list("strychemi", "#{repo_name}").first(10)]
+      recent_commits << [user_repo[1], @github.repos.commits.list(user_repo[0], user_repo[1]).first(10)]
     end
+
+    recent_commits.each do |item|
+      clean_commits = []
+      item[1].each do |commit|
+        clean_commits << [commit["commit"]["committer"],commit["commit"]["message"]]
+      end 
+      item[1] = clean_commits
+    end  
     recent_commits
   end
+
 end
