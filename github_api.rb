c32724e3e0e23d7f7784c9355f6a5bc74b0aec8d
Graham Turner <turnertgraham@gmail.com>
2016-07-20 14:47:15 -0400

    hit rate limit

--- github_api.rb
+++ github_api.rb
@@ -1,12 +1,27 @@
 require 'httparty'
 require 'pp'
-require 'github_api'
 require 'octokit'
 
 class GitHubAPI
-  attr_reader :client
+  include Octokit
 
   def initialize
-    @client = Octokit::Client.new(:access_token => ENV['gh'])
   end
+
+  def list_ten_repos
+    repos = Octokit.repositories("tgturner", per_page: 10, sort: "pushed")
+    list_commit_messages(repos)
+  end
+
+  def list_commit_messages(repos)
+    repos.map do |repo|
+      sleep 0.5
+      Octokit.commits(repo.id, per_page: 10).map { |item| item[:commit][:message] }
+    end
+  end
+
 end
+
+gh = GitHubAPI.new
+pp arr = gh.list_ten_repos
+pp arr[1].length
