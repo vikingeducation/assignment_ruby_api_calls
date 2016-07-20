7a7cf032ba902494e62ca0800a589abf6f7ddb05
Vishal Lama <vishal.lama@gmail.com>
2016-07-20 15:20:29 -0600

    Add #weather_display and other methods

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -8,15 +8,14 @@ class WeatherForecast
 
   BASE_URI = 'http://api.apixu.com/v1/forecast.json'
 
-  def initialize(location = "San Francisco", days = 7)
-    @location = location
-    @days = days
+  def initialize(args = {})
+    @location = args.fetch(:location, "San Francisco")
+    @days = args.fetch(:days, 7)
     @response = get_response
-    @temp_to_string
+    @conditions = conditions
   end
 
   def get_response
-    puts ENV['WEATHER_KEY']
     end_point = BASE_URI + "?key=#{ENV['WEATHER_KEY']}&q=#{location}&days=#{days}"
     response = HTTParty.get(end_point)
 
@@ -32,33 +31,55 @@ class WeatherForecast
 
     end
 
-
-    # end
   end
 
   def lo_temps
     response['forecast']['forecastday'].each_with_object({}) do  |day, object|
       object[day['date']] = day['day']['mintemp_f']
-    
     end
+  end
 
+  def forecast_day
+    response['forecast']['forecastday']
   end
 
   def today
-    pp response['forecast']['forecastday'][0]['day']
+    forecast_day[0]['day']
+  end
+
+  def today_date
+    forecast_day[0]
   end
 
   def tomorrow
-
+    forecast_day[1]['day']
   end
 
-  def set_temp_string
+  def tomorrow_date
+    forecast_day[1]
+  end
+
+  def conditions
     temp_strings = {
       "maxtemp_f" => "Max Temperature in Fahrenheit",
       "mintemp_f" => "Min Temperature in Fahrenheit",
-      "avgtemp_f" => "Average Temp in Fahrenheit"
-                    }
+      "avgtemp_f" => "Average Temp in Fahrenheit",
+      "maxwind_mph" => "Max wind speed (mph)",
+      "totalprecip_in" => "Total precipitation (inches)"
+    }
+    temp_strings
+  end
 
+  def weather_display(day)
+    str = "\nWeather conditions for #{today_date['date']}\n"
+    str << "-" * str.length 
+    str << "\n"
+
+    conditions.each do |key, value|
+      str << "#{value}: #{day[key]}"
+      str << "\n"
+    end
+    str
   end
 
 end
@@ -67,7 +88,12 @@ end
 # http://api.apixu.com/v1/current.json?key=<YOUR_API_KEY>&q=San Francisco&days=7
 # WeatherForecast.new.get_response
 
-puts WeatherForecast.new.today
+wf = WeatherForecast.new
+today = wf.today
+tomorrow = wf.tomorrow
+puts wf.weather_display(today)
+puts wf.weather_display(tomorrow)
+
 # puts WeatherForecast.new.get_response.body.class
 
 # File.open("results.json", "w+") do |f|
