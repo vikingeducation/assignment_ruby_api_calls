a36f842984d38718ec44bc38a20ff6b190bcf5e5
Hannah Squier <hannahsquier@gmail.com>
2016-07-20 14:08:42 -0700

    fix pagination issue

--- github.rb
+++ github.rb
@@ -1,15 +1,19 @@
 require 'octokit'
 require 'pp'
 
-
+Octokit.auto_paginate = true
 client = Octokit::Client.new(:access_token => ENV['GIT_API_KEY'])
 
 
 user = client.user
-user.login
 
+username = user.login
 
 # pp user.all_repositories
-user.rels[:next].get.data.each do |repo|
+count = 0
+Octokit.repos(username).sort_by { |a| a[:created_at] }.each do |repo|
+
   pp repo[:full_name]
+
 end
+
--- weather_forecast.rb
+++ weather_forecast.rb
@@ -18,20 +18,20 @@ class WeatherForecast
   end
 
   def get(response)
-    dates = get_dates(response)
-    hi_temps = get_hi_temps(response)
-    lo_temps = get_lo_temps(response)
-    precip   = get_precip(response)
-    conditions = get_conditions(response)
-    winds = get_max_winds(response)
-    @formatted_location = get_location(response)
+    dates = get_dates
+    hi_temps = get_values('maxtemp_f')
+    lo_temps = get_values('mintemp_f')
+    precip   = get_values('totalprecip_in')
+    conditions = get_conditions
+    winds = get_values('maxwind_mph')
+    @formatted_location = get_location
     @stats = {dates: dates, hi_temps: hi_temps, lo_temps: lo_temps,
       precip: precip, conditions: conditions, winds: winds}
   end
 
   def render(stats)
     puts "*******************"
-    puts "#{@days} Day Weather Forecast for #{@formatted_location}".center(50) if stats[:dates].length > 1
+    puts "#{@days} Day Weather Forecast for #{@formatted_location}".center(@days * 14) if stats[:dates].length > 1
     stats.each do |stat, values|
       row = stat.to_s.rjust(10)
       values.each do |value|
@@ -47,7 +47,8 @@ class WeatherForecast
     @stats.each do |k,v|
       today[k] = [v[0]]
     end
-    today
+    puts "Today's Forecast".center(14)
+    render(today)
   end
 
   def tomorrow
@@ -55,15 +56,15 @@ class WeatherForecast
     @stats.each do |k,v|
       tomorrow[k] = [v[1]]
     end
-
-    tomorrow
+    puts "Tomorrow's Forecast".center(14)
+    render(tomorrow)
   end
 
   private
 
   def get_response
     response = HTTParty.get(build_url).body
-    response = JSON.parse(response)
+    @response = JSON.parse(response)
   end
 
   def build_url
@@ -79,63 +80,39 @@ class WeatherForecast
    raise "Choose number of days between 0 and 10" unless (0..10) === @days
   end
 
-  def get_hi_temps(response)
-    hi_temps = []
+  def get_values(value)
+    values = []
     @days.times do |day|
-      hi_temps << response['forecast']['forecastday'][day]['day']['maxtemp_f']
+      values << @response['forecast']['forecastday'][day]['day'][value]
     end
-    hi_temps
+    values
   end
 
-  def get_lo_temps(response)
-    lo_temps = []
-    @days.times do |day|
-      lo_temps << response['forecast']['forecastday'][day]['day']['mintemp_f']
-    end
-    lo_temps
-  end
-
-  def get_precip(response)
-   precips = []
-   @days.times do |day|
-     precips << response['forecast']['forecastday'][day]['day']['totalprecip_in']
-   end
-   precips
-  end
-
- def get_conditions(response)
+ def get_conditions
    conditions = []
    @days.times do |day|
-     conditions << response['forecast']['forecastday'][day]['day']['condition']['text']
+     conditions << @response['forecast']['forecastday'][day]['day']['condition']['text']
    end
    conditions
  end
 
- def get_max_winds(response)
-   winds = []
-   @days.times do |day|
-     winds << response['forecast']['forecastday'][day]['day']['maxwind_mph']
-   end
-   winds
- end
 
-  def get_dates(response)
+  def get_dates
     dates = []
     @days.times do |day|
-      dates << response['forecast']['forecastday'][day]['date']
+      dates << @response['forecast']['forecastday'][day]['date']
     end
     dates
   end
 
-  def get_location(response)
-    "#{response['location']['name']}, #{response['location']['region']}"
+  def get_location
+    "#{@response['location']['name']}, #{@response['location']['region']}"
   end
 
 end
 
 
-wf = WeatherForecast.new('boise', 5)
+wf = WeatherForecast.new('berkeley', 5)
 today = wf.today
 tomorrow = wf.tomorrow
-wf.render(today)
-wf.render(tomorrow)
+
