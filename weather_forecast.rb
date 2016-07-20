a972baec85a8b8e6f10956c970ba0081c732219f
Graham Turner <turnertgraham@gmail.com>
2016-07-20 13:24:43 -0400

    completed convience methods

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -15,48 +15,63 @@ class WeatherForecast
   def forecast_for(location:)
     path = "#{BASE_URI}q=#{location}&key=#{@key}&days=#{@days}"
     @response = HTTParty.get(path)
+    @forecast = @response["forecast"]["forecastday"]
   end
 
   def high_temp
-    @response["forecast"]["forecastday"].map { |day| day["day"]["maxtemp_f"] }
+    @forecast.map { |day| day["day"]["maxtemp_f"] }
   end
 
   def low_temp
-    @response["forecast"]["forecastday"].map { |day| day["day"]["mintemp_f"] }
+    @forecast.map { |day| day["day"]["mintemp_f"] }
   end
 
   def today
-    day = @response["forecast"]["forecastday"][0]["day"]
+    day = @forecast[0]["day"]
     {
-      date:                 @response["forecast"]["forecastday"][0]["date"], 
+      date:                 @forecast[0]["date"],
       high:                 day["maxtemp_f"],
       low:                  day["mintemp_f"],
-      average:              day["avgtemp_f"], 
-      max_win_speed:        day["maxwind_mph"], 
+      average:              day["avgtemp_f"],
+      max_win_speed:        day["maxwind_mph"],
       total_precipitation:  day["totalprecip_in"],
       condition:            day["condition"]["text"]
     }
   end
 
   def tomorrow
-    day = @response["forecast"]["forecastday"][1]["day"]
+    day = @forecast[1]["day"]
     {
-      date:                 @response["forecast"]["forecastday"][1]["date"],
+      date:                 @forecast[1]["date"],
       high:                 day["maxtemp_f"],
       low:                  day["mintemp_f"],
-      average:              day["avgtemp_f"], 
-      max_win_speed:        day["maxwind_mph"], 
+      average:              day["avgtemp_f"],
+      max_win_speed:        day["maxwind_mph"],
       total_precipitation:  day["totalprecip_in"],
       condition:            day["condition"]["text"]
     }
   end
 
-  def temp_by_hour(day:, hour:)
+  def temp_by_hour(day: 1)
+    @forecast[day - 1]["hour"].map { |hour| "At #{hour["time"]} the temperature will be #{hour["temp_f"]} degrees F" }
+  end
+
+  def sunrise_sunset(day: 1)
+    @forecast[day - 1]["astro"].map { |key, val| "the #{key} is at #{val}" }
+  end
+
+  def will_it_rain(day: 1)
+    @forecast[day - 1]["hour"].map { |hour| "At #{hour["time"]} the chance of rain will be #{hour["will_it_rain"]}" }
+  end
+
 end
 
 w = WeatherForecast.new
-w.forecast_for(location: 92706)
+w.forecast_for(location: "Raleigh, NC")
 pp w.high_temp
 pp w.low_temp
 pp w.today
 pp w.tomorrow
+pp w.temp_by_hour(day: 2)
+pp w.sunrise_sunset(day: 1)
+pp w.will_it_rain(day: 1)
