d29bbe356c540bcdd843a799e16e6b1408f94752
cjvirtucio87 <cjv287@gmail.com>
2016-07-20 11:47:54 -0700

    my refactor

--- weatherforecast.rb
+++ weatherforecast.rb
@@ -2,30 +2,22 @@ module WeatherReportProject
 
   class WeatherForecast
     include HTTParty
-    attr_reader :api_key, :response
-    BASE_URI = "http://api.apixu.com/"
+    attr_reader :response
+    base_uri "http://api.apixu.com/"
     VERSION = "/v1/forecast.json"
 
-
-    API_KEY = ENV["APIXU_KEY"]
-
-    def initialize(loc=nil,days=1)
-      # @options = { query: {key: "#{API_KEY}", q: "#{loc}"} }
-      @response = get_request(loc,days)
+    def initialize
     end
 
-    def get_request(loc,days)
-      # self.class.get("#{VERSION}", @options)
-      self.class.get("#{BASE_URI}#{VERSION}?key=#{API_KEY}&q=#{loc}&days=#{days}")
-      # self.class.get('v1/current.json')
+    def get_request(params)
+      options = { query: params }
+      if params['days'].nil?
+        params['days'] = 1
+      end
+      @response = self.class.get("#{VERSION}", options)
     end
-
-    # http://api.apixu.com/v1/forecast.json?key=eea1db34c14b46a1878161731162007&q=Paris
 
   end
 
 end
 
-w = WeatherReportProject::WeatherForecast.new("Paris", nil)
-
-p w.response
