89b5f4857aaf16cbcc4f40b88b5bacf4784a4fdf
Leo Ahnn <leoahnn@gmail.com>
2016-07-20 14:19:47 -0400

    sorted api calls to repos

--- github_api/github.rb
+++ github_api/github.rb
@@ -1,27 +1,36 @@
 require_relative 'env'
 require 'github_api'
+require 'pp'
+require 'pry'
 
 
 class GithubApi
 
   def initialize(user_name)
     @user_name = user_name
-
+    @repos = send_request
   end
 
 
   def send_request
-    Github.repos.list user: @user_name
-
+    Github.repos.list user: @user_name, sort: "forks", per_page: 10
   end
 
+  def names
+    @repos.map { |repo| repo.name }
+  end
 
-
+  def commit_messages
+    @repos.map do |repo|
+      repo.commits.each { |commit| commit.message }
+    end
+  end
 
 
 end
 
 
 g = GithubApi.new("ChrisGoodson")
-pp g.send_request
- 
+binding.pry
+p g.names
+# p g.commit_messages
