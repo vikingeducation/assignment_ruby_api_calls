2b3ca5d3aeae0865170ef61e4f1bdf3d71f266a4
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-20 12:51:39 -0500

    testest output

--- github_api/github.rb
+++ github_api/github.rb
@@ -1,7 +1,27 @@
 require_relative 'env'
+require 'github_api'
 
 
-class Github
+class GithubApi
+
+  def initialize(user_name)
+    @user_name = user_name
+
+  end
+
+
+  def send_request
+    Github.repos.list user: @user_name
+
+  end
+
+
+
 
 
 end
+
+
+g = GithubApi.new("ChrisGoodson")
+pp g.send_request
+ 
