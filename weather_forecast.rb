16d37e5c0d6ba592c14ab9a5e5cb79217f992fdb
essian <jessicampdennis@hotmail.com>
2016-07-20 18:02:46 +0100

    starts building query

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -2,7 +2,7 @@ require 'httparty'
 
 class WeatherForecast
 
-  END_POINT = 'http://api.apixu.com/v1/forecast.json'
+  BASE_URI = 'http://api.apixu.com/v1/forecast.json'
 
   def initialize(location = "San Francisco", days = 7)
     @location = location
@@ -10,10 +10,17 @@ class WeatherForecast
   end
 
   def get
-
-
+    puts ENV['WEATHER_KEY']
+    end_point = BASE_URI + '?' + 'key=' + ENV['WEATHER_KEY'] + 'q=' + @location
+    response = HTTParty.get(end_point)
+    puts response
   end
 
 
 
 end
+
+
+
+#  http://api.apixu.com/v1/current.json?key=<YOUR_API_KEY>&q=San Francisco&days=7
+WeatherForecast.new.get
