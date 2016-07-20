14db23d22a688dc095459bed029969b4d28ac2ed
Hannah Squier <hannahsquier@gmail.com>
2016-07-20 14:55:44 -0700

    turn sandbox into class

--- github.rb
+++ github.rb
@@ -1,20 +1,34 @@
 require 'octokit'
 require 'pp'
 require 'httparty'
-
-Octokit.auto_paginate = true
-client = Octokit::Client.new(:access_token => ENV['GIT_API_KEY'])
+require 'pry'
 
 
-user = client.user
 
-username = user.login
+class GithubRepos
 
+  def initialize()
+    Octokit.auto_paginate = true
+    @client = Octokit::Client.new(:access_token => ENV['GIT_API_KEY'])
+    @user = @client.user
+    @repos = Octokit.repos(@user.login).sort_by { |a| a[:created_at] }.reverse[0..9]
+  end
 
-repos = Octokit.repos(username).sort_by { |a| a[:created_at] }.reverse[0..9]
-repos.each do |repo|
-  # p repo[:commits_url]
-  p url = repo[:commits_url].gsub('{/sha}', "/#{ENV['GIT_API_KEY']}")
-  # p HTTParty.get(url).body
+  def get_last_ten_names
+    @repos.each do |repo|
+      repo[:full_name]
+    end
+  end
+
+  def get_commit_messages
+    @repos.map do |repo|
+
+      Octokit.commits(repo[:full_name]).map { |commit| commit.commit.message }
+    end
+  end
 end
+
+gh = GithubRepos.new
+p gh.get_commit_messages
+#:commits_url].gsub('{/sha}', "/#{ENV['GIT_API_KEY']}")
 
