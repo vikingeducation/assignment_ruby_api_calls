8ad78b3e147ae20c4e3e449ac4f29bbdaab9eb7b
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-20 13:52:50 -0400

    Work on three convenience methods

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -43,6 +43,23 @@ class WeatherForecast
     end
   end
 
+  def precipitation
+    days = 1
+    request = HTTParty.get("#{BASE_FORECAST_URI}key=#{API_KEY}&q=#{@location}&days=#{days}")
+    p date = request["forecast"]["forecastday"][1][0]
+    p max_temp = request["forecast"]["forecastday"][1]["day"]["will_it_rain"]
+    counter += 1
+  end
+
+
+  def wind
+    request = HTTParty.get("#{BASE_CURRENT_URI}key=#{API_KEY}&q=#{@location}")
+  end
+
+  def conditions
+    request = HTTParty.get("#{BASE_CURRENT_URI}key=#{API_KEY}&q=#{@location}")
+  end
+
   def today
     request = current_for
     puts "Location: #{request["location"]["name"]}"
@@ -53,11 +70,15 @@ class WeatherForecast
     puts "Humidity: #{request["current"]["humidity"]}"
   end
 
-  def tomorrow(day)
+  def tomorrow(day = 1)
     request = forecast_for
-    puts request["forecast"]["forecastday"][day]["date"]
-    puts request["forecast"]["forecastday"][day]["day"]["maxtemp_f"]
-    puts request["forecast"]["forecastday"][day]["day"]["mintemp_f"]
+    puts "Date: " + request["forecast"]["forecastday"][day]["date"].to_s
+    puts "High: " + request["forecast"]["forecastday"][day]["day"]["maxtemp_f"].to_s
+    puts "Low: " + request["forecast"]["forecastday"][day]["day"]["mintemp_f"].to_s
+    puts "Conditions: " + request["forecast"]["forecastday"][day]["day"]["condition"]["text"]
+    puts "Wind: " + request["forecast"]["forecastday"][day]["day"]["maxwind_mph"].to_s
+    puts "Rain: " + request["forecast"]["forecastday"][day]["day"]["totalprecip_in"].to_s
+
   end
 
 end
@@ -67,4 +88,5 @@ w = WeatherForecast.new()
 # pp w.forecast_for
 # w.lo_temps
 # pp w.current_for
-w.tomorrow(1)
+w.tomorrow
+w.precipitation
