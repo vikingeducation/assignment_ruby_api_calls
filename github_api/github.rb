c6f43796ef384931f4afa2608b4dac84110172ae
Vishal Lama <vishal.lama@gmail.com>
2016-07-20 16:42:44 -0600

    Start GitHub API

--- github_api/github.rb
+++ github_api/github.rb
@@ -3,4 +3,17 @@ require 'github_api'
 
 github = Github.new oauth_token: ENV['GITHUB_TOKEN'] 
 
-p github.repos.list.first(1).first.class
+# p github.repos.list.first(1).first.class
+
+# repos = github.repos #.list.first(10)
+# puts repos.commits
+
+commits = github.repos.commits.list 'user-name', 'repo-name'
+puts commits
+
+# repos.each do |repo|
+#   puts repo.class
+#   commits = repo.commits
+#   puts commits
+# end
+
