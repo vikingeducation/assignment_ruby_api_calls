b69dc081137a47c867dd2fb380d7b8e61fc849fe
Hannah Squier <hannahsquier@gmail.com>
2016-07-20 10:26:13 -0700

    add get_hi_temp, get_lo_temp, today, and tomorrow methods

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -4,7 +4,7 @@ require 'httparty'
 require 'prettyprint'
 # http://api.apixu.com/v1/forecast.json?key=fe0fa84dda2a49f28ce160812162007&q=Paris
 class WeatherForecast
-  BASE_URL = 'http://api.apixu.com/v1'
+  BASE_URL = 'http://api.apixu.com/v1/forecast.json?'
   API_KEY = ENV['APIX_API_KEY']
 
   def initialize(loc, days=1)
@@ -16,29 +16,61 @@ class WeatherForecast
 
   def get
     response = HTTParty.get(build_url).body
+    response = JSON.parse(response)
     hi_temps = get_hi_temps(response)
-    low_temps = get_low_temps(response)
-    today = get_today
-    tomorrow = get_tomorrow
-    precipitation = get_precip(response)
+    lo_temps = get_lo_temps(response)
+    # precipitation = get_precip(response)
+    @stats = [hi, lo, prep]
   end
 
   private
+
   def build_url
     loc = URI.encode(@loc)
-    type = @days > 1 ? "forecast" : "current"
-    "#{BASE_URL}/#{type}.json?key=#{API_KEY}&q=#{loc}&days=#{@days}"
+    "#{BASE_URL}key=#{API_KEY}&q=#{loc}&days=#{@days}"
   end
   def validate_location(loc)
 
   end
 
   def validate_num_of_days(num)
-    (0..10) === num
+     raise "Choose number of days between 0 and 10" unless (0..10) === num
+  end
+
+  def get_hi_temps(response)
+    hi_temps = []
+    @days.times do |day|
+      hi_temps << response['forecast']['forecastday'][day]['day']['maxtemp_f']
+    end
+    hi_temps
+  end
+
+   def get_lo_temps(response)
+    lo_temps = []
+    @days.times do |day|
+      lo_temps << response['forecast']['forecastday'][day]['day']['mintemp_f']
+    end
+    lo_temps
+  end
+
+  def today
+    today = []
+    @stats.each do |stat|
+      today << stat[0]
+    end
+    today
+  end
+
+  def tomorrow
+    tomorrow = []
+    @stats.each do |stat|
+      tomorrow << stat[1]
+    end
+    tomorrow
   end
 end
 
 
-wf = WeatherForecast.new('las vegas', 0)
+wf = WeatherForecast.new('las vegas', 11)
 
-pp wf.send_request
+pp wf.get
