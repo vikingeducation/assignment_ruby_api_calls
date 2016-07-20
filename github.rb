ee836ce819f1eb6462f12ae9fc2976efd2a69965
Morgan Martin <morganm5201@gmail.com>
2016-07-20 17:33:33 -0400

    stuff

--- github.rb
+++ github.rb
@@ -1,5 +1,6 @@
 require 'octokit'
 require 'pp'
+require 'httparty'
 
 Octokit.auto_paginate = true
 client = Octokit::Client.new(:access_token => ENV['GIT_API_KEY'])
@@ -9,11 +10,11 @@ user = client.user
 
 username = user.login
 
-# pp user.all_repositories
-count = 0
-Octokit.repos(username).sort_by { |a| a[:created_at] }.each do |repo|
 
-  pp repo[:full_name]
-
+repos = Octokit.repos(username).sort_by { |a| a[:created_at] }.reverse[0..9]
+repos.each do |repo|
+  # p repo[:commits_url]
+  p url = repo[:commits_url].gsub('{/sha}', "/#{ENV['GIT_API_KEY']}")
+  # p HTTParty.get(url).body
 end
 
