8842e279fe93cb97c68f4c6bd60847855fef913f
Mike Lee <asackofwheat@gmail.com>
2016-07-20 13:28:14 -0400

    working on tomorrow

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'json'
+require 'pp'
 
 class WeatherForecast
 
@@ -9,7 +10,7 @@ class WeatherForecast
 
   attr_accessor :location, :days
 
-  def initialize(location = "Paris", days = 1)
+  def initialize(location = "Paris", days = 2)
     @location = location
     @days = days
   end
@@ -19,18 +20,51 @@ class WeatherForecast
   end
 
   def current_for
-    request = HTTParty.get("#{BASE_URI}key=#{API_KEY}&q=#{@location}")
+    request = HTTParty.get("#{BASE_CURRENT_URI}key=#{API_KEY}&q=#{@location}")
   end
 
   def hi_temps
     request = HTTParty.get("#{BASE_FORECAST_URI}key=#{API_KEY}&q=#{@location}&days=#{@days}")
-    p date = request["forecast"]["forecastday"][0]["date"]
-    p max_temp = request["forecast"]["forecastday"][0]["day"]["maxtemp_f"] 
+    counter = 0
+    while counter < @days
+      p date = request["forecast"]["forecastday"][counter]["date"]
+      p max_temp = request["forecast"]["forecastday"][counter]["day"]["maxtemp_f"]
+      counter += 1
+    end
+  end
+
+  def lo_temps
+    request = HTTParty.get("#{BASE_FORECAST_URI}key=#{API_KEY}&q=#{@location}&days=#{@days}")
+    counter = 0
+    while counter < @days
+      p date = request["forecast"]["forecastday"][counter]["date"]
+      p max_temp = request["forecast"]["forecastday"][counter]["day"]["mintemp_f"]
+      counter += 1
+    end
+  end
+
+  def today
+    request = current_for
+    puts "Location: #{request["location"]["name"]}"
+    puts "Last Updated: #{request["current"]["last_updated"]}"
+    puts "Temperature: #{request["current"]["temp_f"]}"
+    puts "Condition: #{request["current"]["condition"]["text"]}"
+    puts "Wind: #{request["current"]["wind_mph"]}"
+    puts "Humidity: #{request["current"]["humidity"]}"
+  end
+
+  def tomorrow(day)
+    request = forecast_for
+    puts request["forecast"]["forecastday"][day]["date"]
+    puts request["forecast"]["forecastday"][day]["day"]["maxtemp_f"]
+    puts request["forecast"]["forecastday"][day]["day"]["mintemp_f"]
   end
 
 end
 
 w = WeatherForecast.new()
 
-#puts w.send_request
-w.hi_temps
+# pp w.forecast_for
+# w.lo_temps
+# pp w.current_for
+w.tomorrow(1)
