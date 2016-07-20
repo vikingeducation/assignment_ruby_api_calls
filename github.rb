437b55807b96437da73fdae733a6a5de0021e5ab
Mike Lee <asackofwheat@gmail.com>
2016-07-20 17:58:33 -0400

    working commits

--- github.rb
+++ github.rb
@@ -7,53 +7,39 @@ class GithubAPI
   GITHUB_API_KEY = ENV["GITHUB_API_KEY"]
 
   def initialize
-    @github = Github.new
+    @github = Github.new(oauth_token: GITHUB_API_KEY)
   end
 
   def user_repos(name)
     @github.repos.list user: name
   end
 
-
-  def ten_latest_repos_display(name)
-    array = []
-    ten_latest_repos(name).each do |repo|
-         array << ["#{repo.created_at}: #{repo.name} : #{repo.html_url}"]
-      end
-    p array.sort.reverse[0..9]
-  end
-
   def ten_latest_repos(name)
     array = []
     response = user_repos(name)
     response.each_page do |page|
-      sleep 0.5
       page.each do |repo|
-        array << repo
+        array << [repo.created_at, repo.name, repo.html_url]
       end
     end
-    p array
+    array.sort.reverse[0..9]
   end
 
   def ten_latest_repo_commits(name)
     hash = {}
-    response = user_repos(name)
-    response.each_page do |page|
-      sleep 0.5
-      page.each do |repo|
-        hash[repo.name] = commits(name, repo.name)
-      end
+    response = ten_latest_repos(name)
+    response.each do |repo|
+      hash[repo[1]] = commits(name, repo[1])
     end
-    p hash
+    hash
   end
 
   def commits(name, repo)
     array = []
     response = @github.repos.commits.list name, repo, sha: ''
     response.each_page do |page|
-      sleep 0.5
       page.each do |repo|
-         array << ["#{repo.commit.message}"]
+         array << "#{repo.commit.message}"
       end
     end
     array.sort.reverse[0..9]
@@ -65,7 +51,7 @@ end
 
 
 g = GithubAPI.new
-# g.ten_latest_repos("asackofwheat")
+# g.ten_latest_repos_display("asackofwheat")
 # pp g.user_repos("asackofwheat")
-g.commits("asackofwheat", "assignment_web_scraper")
-g.ten_latest_repo_commits("lynchd2")
+# p g.commits("asackofwheat", "assignment_web_scraper")
+p g.ten_latest_repo_commits("asackofwheat")
