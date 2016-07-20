9651acae004a1e58ed3b36116cecd3207a95f152
essian <jessicampdennis@hotmail.com>
2016-07-21 00:22:35 +0100

    adds some stuff

--- github_api/github.rb
+++ github_api/github.rb
@@ -1,15 +1,26 @@
 require 'github_api'
-
+require 'pry'
 
 github = Github.new oauth_token: ENV['GITHUB_TOKEN'] 
 
 # p github.repos.list.first(1).first.class
 
-# repos = github.repos #.list.first(10)
-# puts repos.commits
+repos = github.repos.list.sort_by {|repo| repo['created_at']}
 
-commits = github.repos.commits.list 'user-name', 'repo-name'
-puts commits
+# repos_commits = github.repos.list.first(10).each_with_object({}) do |repo, object|
+   
+#    name = repo['name']
+
+#    sha = repo['sha']
+#    commits = github.repos.commits.list('essian', name)
+#    object[name] = commits
+#    binding.pry
+# end
+
+# puts github.repos.commits.list 'essian', 'project_dom_tree'
+
+# commits = github.repos.commits.list 'user-name', 'repo-name'
+# puts commits
 
 # repos.each do |repo|
 #   puts repo.class
