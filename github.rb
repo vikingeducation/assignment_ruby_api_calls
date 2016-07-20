fce7c08b884edc69ad3d71f805c0bcb166f61940
David Jiang <davidjgrey@gmail>
2016-07-20 14:08:25 -0700

    found latest repos

--- github.rb
+++ github.rb
@@ -1,5 +1,5 @@
-#MY_KEY =  b38f1a770afe6558407d5d99bc2d73ec75fba9df
-
+#MY_KEY = b38f1a770afe6558407d5d99bc2d73ec75fba9df
+#KEY_2 = 1fefbd975bf4d10e1a3353d0f28def1ec606ef05
 require 'github_api'
 require 'pp'
 
@@ -11,13 +11,19 @@ class GH_Api
 
   def list_repos
     github = Github.new oauth_token: @key
-    @repos = github.repos.list
+    @repos = github.repos.list sort:"created_at"
   end
 
-  def 
+  def get_latest_repos(number)
+    number.times do |i|
+      puts @repos[i]["name"]
+    end 
+  end
 
 
 end
 
-name = repos[2][:name]
-time_created = repos[2][:created_at]
+# name = repos[2][:name]
+# time_created = repos[2][:created_at]
+#"2016-07-13T16:04:28Z"
+# sorted by date: test = github_test.repos.list sort:"created_at"
