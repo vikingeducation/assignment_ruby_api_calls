4d473180b5b86f136c85d4298cc3e0d0ccfefdb8
Morgan Martin <morganm5201@gmail.com>
2016-07-20 18:05:26 -0400

    add commits

--- github.rb
+++ github.rb
@@ -14,21 +14,33 @@ class GithubRepos
     @repos = Octokit.repos(@user.login).sort_by { |a| a[:created_at] }.reverse[0..9]
   end
 
-  def get_last_ten_names
-    @repos.each do |repo|
-      repo[:full_name]
+  def print_commits
+    last_ten_names = get_last_ten_names
+    get_commit_messages.each_with_index do |repo, index|
+      puts "Last few #{last_ten_names[index]} commits:"
+      repo.each do |message|
+        puts message
+      end
     end
+  end
+
+  def get_last_ten_names
+    names = []
+    @repos.each do |repo|
+      names << repo[:full_name]
+    end
+    names
   end
 
   def get_commit_messages
     @repos.map do |repo|
-
-      Octokit.commits(repo[:full_name]).map { |commit| commit.commit.message }
+      commits = Octokit.commits(repo[:full_name]).map { |commit| commit.commit.message }
     end
   end
 end
 
 gh = GithubRepos.new
-p gh.get_commit_messages
+# p gh.get_commit_messages
+gh.print_commits
 #:commits_url].gsub('{/sha}', "/#{ENV['GIT_API_KEY']}")
 
