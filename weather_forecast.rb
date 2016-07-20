fe6da1e825fdb08c6b5a9e2a215c24b6504d7667
Vishal Lama <vishal.lama@gmail.com>
2016-07-20 12:03:59 -0600

    Some more updates

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -1,26 +1,51 @@
 require 'httparty'
+require 'pp'
 
 class WeatherForecast
+
+  attr_reader :location, :days, :response
 
   BASE_URI = 'http://api.apixu.com/v1/forecast.json'
 
   def initialize(location = "San Francisco", days = 7)
     @location = location
     @days = days
+    @response = get_response
   end
 
-  def get
+  def get_response
     puts ENV['WEATHER_KEY']
-    end_point = BASE_URI + '?' + 'key=' + ENV['WEATHER_KEY'] + 'q=' + @location
+    end_point = BASE_URI + "?key=#{ENV['WEATHER_KEY']}&q=#{location}&days=#{days}"
     response = HTTParty.get(end_point)
-    puts response
+    response.parsed_response
   end
 
+  def hi_temps
+    response["maxtemp_f"]
+  end
 
+  def lo_temps
+
+  end
+
+  def today
+
+  end
+
+  def tomorrow
+
+  end
 
 end
 
 
+# http://api.apixu.com/v1/current.json?key=<YOUR_API_KEY>&q=San Francisco&days=7
+# WeatherForecast.new.get_response
 
-#  http://api.apixu.com/v1/current.json?key=<YOUR_API_KEY>&q=San Francisco&days=7
-WeatherForecast.new.get
+puts WeatherForecast.new.hi_temps
+# puts WeatherForecast.new.get_response.body.class
+
+# File.open("results.json", "w+") do |f|
+#   f.write(JSON.parse(WeatherForecast.new.get_response.to_s))
+# end
+
