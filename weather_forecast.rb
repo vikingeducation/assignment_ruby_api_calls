cccf38a2e43a6ade9d0979be691e2eae9e95b237
David Jiang <davidjgrey@gmail>
2016-07-20 10:28:06 -0700

    getting forecast data

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -10,6 +10,76 @@ class WeatherForecast
     @days = days
   end
 
+  def generate_forecast
+    forecast_data = get
+    hi_temps = get_hi_temps(forecast_data)
+    low_temps = get_low_temps(forecast_data)
+    conditions = get_conditions(forecast_data)
+    precipitation = get_precipitation(forecast_data)
+    sunrise = get_sunrise(forecast_data)
+    sunset = get_sunset(forecast_data)
+  end
+
+  def get_sunset(forecast_data)
+    sunsets = {}
+     @days.to_i.times do |day|
+      date = forecast_data["forecast"]["forecastday"][day]["date"]
+      sunset = forecast_data["forecast"]["forecastday"][0]["astro"]["sunset"]
+      sunsets[date] = sunset
+    end
+    sunsets
+  end
+
+  def get_sunrise(forecast_data)
+    sunrises = {}
+    @days.to_i.times do |day|
+      date = forecast_data["forecast"]["forecastday"][day]["date"]
+      sunrise = forecast_data["forecast"]["forecastday"][day]["astro"]["sunrise"]
+      sunrises[date] = sunrise
+    end
+    sunrises
+  end  
+
+  def get_precipitation(forecast_data)
+    precipitaion = {}
+    @days.to_i.times do |day|
+      date = forecast_data["forecast"]["forecastday"][day]["date"]
+      inches = forecast_data["forecast"]["forecastday"][day]["day"]["totalprecip_in"]
+      precipitaion[date] = inches
+    end
+    precipitaion
+  end
+
+  def get_conditions(forecast_data)
+    conditions = {}
+    @days.to_i.times do |day|
+      date = forecast_data["forecast"]["forecastday"][day]["date"]
+      condition = forecast_data["forecast"]["forecastday"][day]["day"]["condition"]["text"]
+      conditions[date] = condition
+    end
+    conditions
+  end
+
+  def get_low_temps(forecast_data)
+    low_temps = {}
+    @days.to_i.times do |day|
+      date = forecast_data["forecast"]["forecastday"][day]["date"]
+      low_temp = forecast_data["forecast"]["forecastday"][day]["day"]["mintemp_f"]
+      low_temps[date] = low_temp
+    end
+    low_temps
+  end
+
+  def get_hi_temps(forecast_data)
+    hi_temps = {}
+    @days.to_i.times do |day|
+      date = forecast_data["forecast"]["forecastday"][day]["date"]
+      hi_temp = forecast_data["forecast"]["forecastday"][day]["day"]["maxtemp_f"]
+      hi_temps[date] = hi_temp
+    end
+    hi_temps
+  end
+
   def get
     params = {"q" => @location, "days" => @days}
     url = build_url(params)
@@ -31,3 +101,16 @@ class WeatherForecast
   end
 
 end
+
+# date = response["forecast"]["forecastday"][0]["date"]
+# high = response["forecast"]["forecastday"][0]["day"]["maxtemp_f"]
+# low =response["forecast"]["forecastday"][0]["day"]["mintemp_f"]
+# descripion = response["forecast"]["forecastday"][0]["day"]["condition"]["text"]
+# precipitation = response["forecast"]["forecastday"][0]["day"]["totalprecip_in"]
+# sunrise = response["forecast"]["forecastday"][0]["astro"]["sunrise"]
+# sunset = response["forecast"]["forecastday"][0]["astro"]["sunset"]
+# for tomorrow, use 1 after forecastday
+
+
+
+
