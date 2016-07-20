49906849e99adc42be7002f9d4604c9204e080a3
Alex Lach <alexglach@gmail.com>
2016-07-20 14:08:39 -0400

    done with weather

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -1,93 +1,114 @@
 require 'httparty'
 require 'uri'
+require 'pry'
 
 
 class WeatherForecast
   ENDPOINT = "http://api.apixu.com/v1/forecast.json?"
 
-  def initialize(location = "98109", days = "5")
-    @location = location
-    @days = days
+  attr_reader :hi_temps, :low_temps, :conditions, :precipitation, :sunrise, :sunset
+
+  def initialize(key)
+    @key = key
   end
 
-  def generate_forecast
-    forecast_data = get
-    hi_temps = get_hi_temps(forecast_data)
-    low_temps = get_low_temps(forecast_data)
-    conditions = get_conditions(forecast_data)
-    precipitation = get_precipitation(forecast_data)
-    sunrise = get_sunrise(forecast_data)
-    sunset = get_sunset(forecast_data)
+  def generate_forecast(params)
+    
+    forecast_data = get(params)
+    @hi_temps = get_hi_temps(forecast_data)
+    @low_temps = get_low_temps(forecast_data)
+    @conditions = get_conditions(forecast_data)
+    @precipitation = get_precipitation(forecast_data)
+    @sunrise = get_sunrise(forecast_data)
+    @sunset = get_sunset(forecast_data)
+    @all_info = {:hi_temps => @hi_temps, :low_temps => @low_temps, :conditions => @conditions, :precipitation => @precipitation, :sunrise => @sunrise, :sunset => @sunset}
+  end
+
+  def today
+    today_info = {}
+    @all_info.each do |key, value|
+      today_info[key] = value[0][1]
+    end
+    today_info
+  end
+
+  def tomorrow
+    tomorrow_info = {}
+    @all_info.each do |key, value|
+      tomorrow_info[key] = value[1][1]
+    end
+    tomorrow_info
   end
 
   def get_sunset(forecast_data)
-    sunsets = {}
+    sunsets = []
      @days.to_i.times do |day|
       date = forecast_data["forecast"]["forecastday"][day]["date"]
       sunset = forecast_data["forecast"]["forecastday"][0]["astro"]["sunset"]
-      sunsets[date] = sunset
+      sunsets << [date, sunset]
     end
     sunsets
   end
 
   def get_sunrise(forecast_data)
-    sunrises = {}
+    sunrises = []
     @days.to_i.times do |day|
       date = forecast_data["forecast"]["forecastday"][day]["date"]
       sunrise = forecast_data["forecast"]["forecastday"][day]["astro"]["sunrise"]
-      sunrises[date] = sunrise
+      sunrises << [date, sunrise]
     end
     sunrises
   end  
 
   def get_precipitation(forecast_data)
-    precipitaion = {}
+    precipitation = []
     @days.to_i.times do |day|
       date = forecast_data["forecast"]["forecastday"][day]["date"]
       inches = forecast_data["forecast"]["forecastday"][day]["day"]["totalprecip_in"]
-      precipitaion[date] = inches
+      precipitation << [date, inches]
     end
-    precipitaion
+    precipitation
   end
 
   def get_conditions(forecast_data)
-    conditions = {}
+    conditions = []
     @days.to_i.times do |day|
       date = forecast_data["forecast"]["forecastday"][day]["date"]
       condition = forecast_data["forecast"]["forecastday"][day]["day"]["condition"]["text"]
-      conditions[date] = condition
+      conditions << [date, condition]
     end
     conditions
   end
 
   def get_low_temps(forecast_data)
-    low_temps = {}
+    low_temps = []
     @days.to_i.times do |day|
       date = forecast_data["forecast"]["forecastday"][day]["date"]
       low_temp = forecast_data["forecast"]["forecastday"][day]["day"]["mintemp_f"]
-      low_temps[date] = low_temp
+      low_temps << [date, low_temp]
     end
     low_temps
   end
 
   def get_hi_temps(forecast_data)
-    hi_temps = {}
+    hi_temps = []
     @days.to_i.times do |day|
       date = forecast_data["forecast"]["forecastday"][day]["date"]
       hi_temp = forecast_data["forecast"]["forecastday"][day]["day"]["maxtemp_f"]
-      hi_temps[date] = hi_temp
+      hi_temps << [date, hi_temp]
     end
     hi_temps
   end
 
-  def get
-    params = {"q" => @location, "days" => @days}
+  def get(params)
+    params = params.map {|k, v| [k, v.to_s]}.to_h
+    @days = params[:days]
     url = build_url(params)
     response = HTTParty.get(url)
   end
 
   def build_url(params)
-    query_string = "key=#{ENV["WEATHER_KEY"]}&"
+    query_string = "key=#{@key}&"
     query_string += build_query_string(params)
     "#{ENDPOINT}#{query_string}"
 
