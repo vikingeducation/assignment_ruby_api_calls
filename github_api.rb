3ee8f9e6a8c6a33a3deaf21793eb3f13b953e177
Adrian Mui <adrian.mui@gmail.com>
2016-07-20 18:03:46 -0400

    merge

--- github_api.rb
+++ github_api.rb
@@ -1,15 +1,31 @@
 require 'github_api'
+require 'pry'
 
 
 class GitHubApiProject
 
   def initialize(username)
     @username = username
+    @github = Github.new o_auth_token: '18a089a07367243efef49fdff6c98d87f15870ea'
   end
 
   def get_latest_10
-    repos = github.repos.list user: @username
+    repos = @github.repos.list user: @username ,sort: 'created'
+
+    binding.pry
     repos.body
+   
+  end
+
+  def get_latest_10_messages
+    agent = Github.new o_auth_token: '18a089a07367243efef49fdff6c98d87f15870ea'
+    repos = agent.commits.list  'adrianmui', 'assignment_web_scraper'
+     binding.pry
+
+     repos
   end
 
 end
+
+g = GitHubApiProject.new('adrianmui')
+p g.get_latest_10
