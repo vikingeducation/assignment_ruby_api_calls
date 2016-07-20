01f60ee95cd2c39ac49da6a5b56c728f2946d720
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-20 17:34:44 -0400

    Work on ten latest messages for ten repos

--- github.rb
+++ github.rb
@@ -4,6 +4,8 @@ require 'pry'
 
 class GithubAPI
 
+  GITHUB_API_KEY = ENV["GITHUB_API_KEY"]
+
   def initialize
     @github = Github.new
   end
@@ -13,26 +15,48 @@ class GithubAPI
   end
 
 
+  def ten_latest_repos_display(name)
+    array = []
+    ten_latest_repos(name).each do |repo|
+         array << ["#{repo.created_at}: #{repo.name} : #{repo.html_url}"]
+      end
+    p array.sort.reverse[0..9]
+  end
+
   def ten_latest_repos(name)
     array = []
     response = user_repos(name)
     response.each_page do |page|
+      sleep 0.5
       page.each do |repo|
-         array << ["#{repo.created_at}: #{repo.name} : #{repo.html_url}"]
+        array << repo
       end
     end
-    p array.sort.reverse[0..9]
+    p array
+  end
+
+  def ten_latest_repo_commits(name)
+    hash = {}
+    response = user_repos(name)
+    response.each_page do |page|
+      sleep 0.5
+      page.each do |repo|
+        hash[repo.name] = commits(name, repo.name)
+      end
+    end
+    p hash
   end
 
   def commits(name, repo)
     array = []
     response = @github.repos.commits.list name, repo, sha: ''
     response.each_page do |page|
+      sleep 0.5
       page.each do |repo|
          array << ["#{repo.commit.message}"]
       end
     end
-    p array.sort.reverse[0..9]
+    array.sort.reverse[0..9]
   end
 
 
@@ -44,3 +68,4 @@ g = GithubAPI.new
 # g.ten_latest_repos("asackofwheat")
 # pp g.user_repos("asackofwheat")
 g.commits("asackofwheat", "assignment_web_scraper")
+g.ten_latest_repo_commits("lynchd2")
