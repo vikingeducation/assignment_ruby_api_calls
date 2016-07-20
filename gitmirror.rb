13a1e220b47053b086e1ffa034c41eeecb16c9df
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-07-20 15:33:59 -0700

    Finding commits

--- gitmirror.rb
+++ gitmirror.rb
@@ -1,31 +1,44 @@
 require 'httparty'
 require 'pp'
 require 'octokit'
+require 'pry'
 
 class GitMirror
   include Octokit
 
-  def initialize
+  def initialize(user = "tgturner")
     @client = Octokit::Client.new(access_token: ENV['gh'])
-
+    @user = user
   end
 
   def get_forked_repos
-    repos = @client.repositories("tgturner", sort: "pushed")
-    repos.select{ |repo| repo[:owner][:fork] == true }
+    repos=@client.repositories(@user, sort: "pushed", per_page: 100).select{ |repo| repo.fork == true }
     # list_commit_messages(repos)
 
   end
 
   def list_commit_messages(repos)
     repos.map do |repo|
+      print "."
       sleep 0.5
-      @client.commits(repo.id, per_page: 10).map { |item| item[:commit][:message] }
+
+      @client.commits(repo.id).map do |item| 
+        begin
+        if login = item[:author][:login] == @user
+          item[:commit][:author][:date]
+        else
+          "Wrong!"
+        end
+        rescue
+          binding.pry
+        end
+      end
+
     end
   end
 
 end
 
 gh = GitMirror.new
-pp arr = gh.get_forked_repos
-# pp arr[1].length
+arr = gh.get_forked_repos
+pp gh.list_commit_messages(arr)
