7e306d9c29698ba3c619133fe637f6a77ad396dd
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-20 12:03:23 -0500

    added forcast methods

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -5,6 +5,8 @@ require 'pp'
 
 class WeatherForecast
 
+
+
 # days 1-10, location city ST or zipcode
   def initialize(location = '22180', days = 1)
     @days = days.to_s
@@ -17,21 +19,41 @@ class WeatherForecast
     uri.join('&')
   end
 
+  def send_request
+    @request = HTTParty.new.get(build_uri)
+
+  end
+
+
 
   def hi_temps
     # forecast.forecastday.day.maxtemp_f
+    @request["forecast"]["forecastday"].map {|date|  date["day"]["maxtemp_f"]    }
   end
 
   def low_temps
     # forecast.forecastday.day.mintemp_f
+    @request["forecast"]["forecastday"].map {|date|  date["day"]["mintemp_f"]    }
   end
 
   def today
     # current.temp_f
+    #hi_
+    #lo_
+    current_spot = @request["forecast"]["forecastday"][0]["day"]
+    @request["current"]["temp_f"]
+    current_spot["maxtemp_f"]
+    current_spot["mintemp_f"]
+    current_spot["condition"]
+
+
   end
 
   def tomorrow
-    #
+    current_spot = @request["forecast"]["forecastday"][1]["day"]
+    current_spot["maxtemp_f"]
+    current_spot["mintemp_f"]
+    current_spot["condition"]
   end
 
 
@@ -41,6 +63,6 @@ end
 
 
 w_api = WeatherForecast.new('bloomington in', 3)
-uri = w_api.build_uri
-h = HTTParty.get(uri)
-pp h
+w_api.send_request
+p w_api.hi_temps
+
