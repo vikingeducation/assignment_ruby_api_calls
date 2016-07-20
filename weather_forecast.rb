629aa2bc38b89217b84e66794341c9fdd9ecb1bf
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-20 13:03:52 -0400

    Work on forecast

--- weather_forecast.rb
+++ weather_forecast.rb
@@ -3,26 +3,34 @@ require 'json'
 
 class WeatherForecast
 
-  BASE_URI = "http://api.apixu.com/v1/current.json?"
-
+  BASE_CURRENT_URI = "http://api.apixu.com/v1/current.json?"
+  BASE_FORECAST_URI = "http://api.apixu.com/v1/forecast.json?"
   API_KEY = ENV["API_KEY"]
 
-  attr_accessor :location
+  attr_accessor :location, :days
 
-  def initialize(location = "Paris", days = 7)
+  def initialize(location = "Paris", days = 1)
     @location = location
     @days = days
   end
 
-  def forecast_for(location)
+  def forecast_for
+    request = HTTParty.get("#{BASE_FORECAST_URI}key=#{API_KEY}&q=#{@location}&days=#{@days}")
   end
 
-  def send_request
+  def current_for
     request = HTTParty.get("#{BASE_URI}key=#{API_KEY}&q=#{@location}")
+  end
+
+  def hi_temps
+    request = HTTParty.get("#{BASE_FORECAST_URI}key=#{API_KEY}&q=#{@location}&days=#{@days}")
+    p date = request["forecast"]["forecastday"][0]["date"]
+    p max_temp = request["forecast"]["forecastday"][0]["day"]["maxtemp_f"] 
   end
 
 end
 
-# w = WeatherForecast.new("Tarpon_Springs")
+w = WeatherForecast.new()
 
-# puts w.send_request
+#puts w.send_request
+w.hi_temps
