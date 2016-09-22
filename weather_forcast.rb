1baec1bc67c7c0c49aaa34d2ba6765595f0ffceb
Joe Bernardi <joebernardi@hotmail.com>
2016-09-22 15:13:30 -0500

    ny times

---
+++ weather_forcast.rb
@@ -0,0 +1,44 @@
+# API call:
+
+http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY}
+Parameters:
+
+APPID {APIKEY} is your unique API key
+Example of API call:
+
+# server api.openweathermap.org
+# call API by city ID not name city coordiantes or zip
+
+
+# call by city name -- api.openweathermap.org/data/2.5/weather?q={city name}
+
+# Format
+Description:
+
+JSON format is used by default. To get data in XML or HTML formats just set up mode = xml or html.
+
+Parameters:
+
+mode - possible values are xml and html. If mode parameter is empty the format is JSON by default.
+Examples of API calls:
+
+JSON api.openweathermap.org/data/2.5/weather?q=London
+
+XML api.openweathermap.org/data/2.5/weather?q=London&mode=xml
+
+HTML api.openweathermap.org/data/2.5/weather?q=London&mode=html
+
+# call by city name and days
+
+API call:
+
+api.openweathermap.org/data/2.5/forecast/daily?q={city name},{country code}&cnt={cnt}
+Parameters:
+
+q city name and country code divided by comma, use ISO 3166 country codes
+
+cnt number of days returned (from 1 to 16)
+
+Examples of API calls:
+
+Call 7 days forecast by city name in XML format and metric units api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=metric&cnt=7
