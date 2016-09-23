3ba51f45251a25290162074f15379108f88d6377
Joe Bernardi <joebernardi@hotmail.com>
2016-09-23 14:25:37 -0500

    working through response

--- github/github.rb
+++ github/github.rb
@@ -1,5 +1,5 @@
 # base https://api.github.com
-
+require 'pry-byebug'
 
 # https://api.github.com/?access_token=OAUTH-TOKEN
 
@@ -7,25 +7,41 @@
 require 'github_api'
 
 
-class Github
+class Github_api
 
 
 	def initialize
 
+		@repos = nil
+
 		@github = Github.new  oauth_token: ENV["GITHUB"],
-													user_agent: 'jdbernardi'
+													user_agent: 'jdbernardi',
+													auto_pagination: true
 													binding.pry
 	end
 
 
 	def get_repositories
 
-		repos = @github.repos.list user: 'jdbernardi'
+		@repos = @github.repos.list user: 'jdbernardi'#, { |repo| repo.name } -- error
+		@repos = @repos[1..10]
+		binding.pry
+	end
+
+
+	def get_commits
+binding.pry
+		@repos.each do | r |
+
+			@commits = @github.repos.commits.list 'jdbernardi', r.name
+
+		end
 binding.pry
 	end
 
 
 end
 
-git = Github.new
+git = Github_api.new
 git.get_repositories
+git.get_commits
