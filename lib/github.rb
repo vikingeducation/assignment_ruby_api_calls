77e43a49538a8f51989aed6911f3b10fd33b9769
Sampson Crowley <sampsonsprojects@gmail.com>
2016-11-15 13:31:39 -0700

    OOP-ified

--- lib/github.rb
+++ lib/github.rb
@@ -5,29 +5,74 @@ require 'json'
 
 Figaro.application = Figaro::Application.new(
   path: File.expand_path("./config/application.yml")
-)
+  )
 Figaro.load
 
+class GithubReader
 
-github = Github.new oauth_token: ENV['GITHUB_KEY']
-sleep(1)
+  attr_reader :github, :user
 
-repos = github.repos.list.sort_by{ |k| k.created_at }.reverse
-users = github.users.get
-
-repos[0..9].each do |repo|
-  puts
-  puts "------------------------------------"
-  puts repo.name
-  puts "------------------------------------"
-  puts
-  commits = github.repos.commits.list(users.login, repo.name)
-  commits = commits.map do |commit|
-              { message: commit.commit.message,
-                date: commit.commit.created_at }
-            end.sort_by{ |k| k[:date] }.reverse
-
-  commits[0..9].each do |commit|
-    puts "\t" + commit[:message]
+  def initialize
+    @github = Github.new(oauth_token: ENV['GITHUB_KEY'])
+    sleep(0.5)
+    @user = github.users.get.login
+    sleep(0.5)
   end
+
+  def recent_repos
+    parse_repos(get_repos)
+  end
+
+  def pretty_print
+    puts "\n\n"
+    recent_repos.each do |repo|
+      puts
+      puts "------------------------------------"
+      puts repo[:name]
+      puts "------------------------------------"
+      puts
+      repo[:commits].each do |commit|
+        puts "\t" + commit[:message]
+      end
+    end
+    puts "\n\n"
+  end
+
+  private
+
+    def get_repos
+      repos = github.repos.list
+      sleep(0.5)
+      num = repos.length > 9 ? 10 : repos.length
+      repos. sort_by{ |k| k.created_at }.last(num)
+    end
+
+    def parse_repos(repo_list)
+      repo_list.map do |repo|
+        commits = get_commits(repo.name)
+
+        {
+          name: repo.name,
+          commits:commits
+        }
+      end
+    end
+
+    def get_commits(repo)
+      commits = github.repos.commits.list(user, repo)
+      sleep(0.5)
+      commits = commits.map do |commit|
+        {
+          message: commit.commit.message,
+          date: commit.commit.author.date
+        }
+      end.sort_by{ |k| k[:date] }
+      num = commits.length > 10 ? 10 : commits.length
+      commits.last(10)
+    end
+
+
 end
+GithubReader.new.pretty_print
+
+
