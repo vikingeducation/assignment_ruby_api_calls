a98a3fd36398bce01651bd1bfb8830b295a90482
Kenneth Kottenstette <kennykotten@gmail.com>
2016-11-15 12:00:21 -0800

    first version working

---
+++ owm_api/render_day.rb
@@ -0,0 +1,17 @@
+class Render
+  def display_highs
+    #
+  end
+
+  def display_lows
+    #
+  end
+
+  def display_today
+    #
+  end
+
+  def display_tomorrow
+    #
+  end
+end
--- owm_api/weather_forecast.rb
+++ owm_api/weather_forecast.rb
@@ -4,27 +4,30 @@ require 'json'
 
 require_relative 'example' # sets API key environmental variable
 require_relative 'day' # stores information on a day based on JSON data from OWM
+require_relative 'render' #outputs to CLI
 
 class WeatherForecast # using Open Weather Map
 
-  def initialize location="San Francisco", days=1
-    raise ArgumentError if days < 1 || days > 16
+  def initialize location="San Francisco", day_count=1
+    raise ArgumentError if day_count < 1 || day_count > 16
 
     @location = location.downcase
-    @days = days
+    @day_count = day_count
     @key = ENV["OWM_KEY"]
+    @all_days = []
 
     request_url
     send_request
     parse_JSON
     get_days
+    render
   end
 
   def request_url
     location = @location.gsub(/\s+/, "+")
     @url = "http://api.openweathermap.org/data/2.5/forecast/daily?q="
     @url += "#{location}" # adds location
-    @url += "&cnt=#{@days}" # adds day count
+    @url += "&cnt=#{@day_count}" # adds day count
     @url += "&units=imperial" # sets imperial units
     @url += "&appid=#{@key}" # adds api key
   end
@@ -38,11 +41,31 @@ class WeatherForecast # using Open Weather Map
   end
 
   def get_days
-    days = @response_json["list"]
-    days.each do |day|
-
+    days_json = @response_json["list"]
+    days_json.each do |day_json|
+      @all_days << Day.new(day_json)
     end
-    pp @response_json["list"]
+  end
+
+  def render
+    puts "Press 1 for high temperatures"
+    puts "Press 2 for low temperatures"
+    puts "Press 3 for todays's temperature"
+    puts "Press 4 for tomorrow's temperature"
+    
+    response = gets.chomp.to_i
+    case response
+    when 1
+      Render.display_highs(@all_days)
+    when 2
+      Render.display_lows(@all_days)
+    when 3
+      Render.display_day(@all_days[0])
+    when 4
+      Render.display_day(@all_days[1])
+    else
+      "Enter a valid number!"
+    end
   end
 
 end
