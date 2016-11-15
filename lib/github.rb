812be8764c3d97ef916c74f52612310aed6ce864
Sampson Crowley <sampsonsprojects@gmail.com>
2016-11-15 12:37:00 -0700

    basic github

--- lib/github.rb
+++ lib/github.rb
@@ -8,11 +8,20 @@ Figaro.application = Figaro::Application.new(
 )
 Figaro.load
 
-github = Github.new  oauth_token: ENV['GITHUB_KEY']
 
- repos = github.repos.list.sort_by{ |k| k.created_at }.reverse
+github = Github.new oauth_token: ENV['GITHUB_KEY']
+sleep(1)
+repos = github.repos.list.sort_by{ |k| k.created_at }.reverse
+users = github.users.get
+username = ''
+users.each do |user|
+  username = user[1]
+  break
+end
+p username
 
- repos[0..9].each do |repo|
-   puts repo.name
-   puts repo.created_at
- end
+repos[0..9].each do |repo|
+  puts repo.name
+  commits = github.repos.commits.list(github.user, repo.name)
+  commits
+end
