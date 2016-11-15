8fcd59e23e2ad0bd6df1129d333eea9f1977935f
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-11-15 13:15:24 -0500

    Continue api calls

--- lib/weather_forecast.rb
+++ lib/weather_forecast.rb
@@ -4,21 +4,41 @@ class WeatherForecast
 
   BASE_URI = "http://api.openweathermap.org/data/2.5/forecast" # {city name},{country code}
   API_KEY = ENV['WEATHER_KEY']
-  def initialize(city_id_mappings, location = "Cupertino", num_days = 5)
-    @location = location
-    @num_days = num_days
-    @http_client = HTTParty
-    @mode = mode
-    @city_id_mappings =
+
+  def initialize(args = {})
+    @location         = args.fetch(:location, 'cupertino')
+    @num_days         = args.fetch(:days, 5)
+    @http_client      = args.fetch(:http_client, HTTParty)
+    @city_id_mappings = args.fetch(:city_id_mappings, nil)
   end
 
   def get_forecast
-    endpoint = "#{BASE_URI}?id=#{location_id}&appid=#{API_KEY}"
-    @http_client.get(endpoint)
+    http_client.get(BASE_URI, query: { id: 524901, appid: API_KEY })
+    #http_client.get(BASE_URI, query: { id: location_id, appid: API_KEY })
   end
 
   def location_id
-    @city_id_mappings[@location]
+    city_id_mappings[location]
+  end
+
+  private
+    attr_reader :city_id_mappings, :http_client, :location
+end
+
+# ------------------------------------------------
+
+class JSONParser
+
+  def parse(json)
+    JSON.parse('city_list.json')
   end
 
 end
+
+city_id_file_path = File.expand_path('city_list.json', File.dirname(__FILE__))
+
+city_id_json = File.open(city_id_file_path).read
+
+p city_id_json
+
+#p JSONParser.new.parse(city_id_json)
