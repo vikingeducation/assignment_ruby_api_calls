b1ff099530f436b39f0d445046b68270633b2a13
John Sawyer <sawyer@sawyermerchant.com>
2016-11-15 15:36:15 -0500

    Add yaml

--- example.rb
+++ example.rb
@@ -1,5 +1,6 @@
 require_relative 'lib/get_weather_forecast'
 require_relative 'lib/parse_forecast'
+require 'yaml'
 
 Figaro.application = Figaro::Application.new(
     environment: 'development',
@@ -15,12 +16,17 @@ weather_api = GetWeatherForecast.new( 33603, 3, ENV['weather_api'])
 forecast = weather_api.get
 
 
+File.open('data/forecast.yaml', 'w') do |f|
+  dump = forecast.to_yaml
+  f.write(dump)
+end
+
 # File.open('data/forecast.json', 'w') do |f|
 #   json = JSON.pretty_generate(forecast)
 #   f.write(json)
 # end
 
 
-ParseForecast.new('data/forecast.json')
+# ParseForecast.new('data/forecast.json')
 
 
