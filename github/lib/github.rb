b605f1dda2a78b6a952c0895a948e8f3b8709421
John Sawyer <sawyer@sawyermerchant.com>
2016-11-16 09:20:13 -0500

    Move reader into a class

--- github/lib/github.rb
+++ github/lib/github.rb
@@ -3,25 +3,33 @@ require 'httparty'
 require 'pry'
 require 'pry-byebug'
 
-github = Github.new(oauth_token: ENV["Github_API"])
 
-user = 'sawyermerchant'
+class GithubReader
 
-github.repos.list(per_page: 10) do |repo|
-  # binding.pry
-  puts repo.name
-  github.repos.commits.list(user, repo.name) do  |commit|
-    message = commit.commit.message
-    date = commit.commit.author.date
+  attr_reader :github, :user
 
-    puts "Date #{date}, Message #{message}"
+  def initialize
+    @github = Github.new(oauth_token: ENV["Github_API"])
+    @user = 'sawyermerchant'
+  end
+
+  def get_repos
+    @github.repos.list(per_page: 10) do |repo|
+      sleep(0.1)
+      # binding.pry
+      puts repo.name
+      github.repos.commits.list(user, repo.name) do  |commit|
+        message = commit.commit.message
+        date = commit.commit.author.date
+
+        puts "Date #{date}, Message #{message}"
+      end
+    end
   end
 
 end
 
 
+g = GithubReader.new
 
-
-# commits = commits.each do |commit|
-#           message = commit.commit.message,
-#           date = commit.commit.author.date
+g.get_repos
