eab03b047d93a07238ae6ad5947e5bc44ba4e54d
John Sawyer <sawyer@sawyermerchant.com>
2016-11-15 19:28:42 -0500

    Print 10 repos and 10 commit messages

--- github/lib/github.rb
+++ github/lib/github.rb
@@ -5,14 +5,18 @@ require 'pry-byebug'
 
 github = Github.new(oauth_token: ENV["Github_API"])
 
+user = 'sawyermerchant'
 
 github.repos.list(per_page: 10) do |repo|
   # binding.pry
   puts repo.name
-  p repo.commit    #.message
-  # repo.commits.list do |e|
-  #   put repo.commits
-  # end
+  github.repos.commits.list(user, repo.name) do  |commit|
+    message = commit.commit.message
+    date = commit.commit.author.date
+
+    puts "Date #{date}, Message #{message}"
+  end
+
 end
 
 
