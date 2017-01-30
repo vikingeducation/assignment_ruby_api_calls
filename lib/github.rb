c2f9dd03678e5a761f78975f05120f2b94292cb1
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-01-30 18:20:07 +0800

    Clean up github

--- lib/github.rb
+++ lib/github.rb
@@ -7,14 +7,14 @@ repos = github.repos.list(user: 'yxlau', sort: 'updated')
 puts "Latest Repos:"
 repos.each do |r|
   puts r.name.upcase
-  sleep 2
+  sleep 1
   commits = github.repos.commits.list('yxlau', r.name)
   counter = 0
   commits.each do |c|
     break unless c
     break if counter == 10
     puts "  Date: #{c['commit']['author']['date']}"
-    puts "    #{c['commit']['message']}".rjust(10)
+    puts "    #{c['commit']['message']}"
     counter += 1
   end
   puts
