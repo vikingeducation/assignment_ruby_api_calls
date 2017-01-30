184ad26c6c0db439ed767888060aa39b20fa13db
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-01-30 18:32:15 +0800

    Add class wrapper

--- lib/github.rb
+++ lib/github.rb
@@ -1,21 +1,28 @@
 require 'github_api'
 
-github = Github.new(oauth_token: ENV['GH'], per_page: 10)
+class GithubRepos
 
-repos = github.repos.list(user: 'yxlau', sort: 'updated')
-
-puts "Latest Repos:"
-repos.each do |r|
-  puts r.name.upcase
-  sleep 1
-  commits = github.repos.commits.list('yxlau', r.name)
-  counter = 0
-  commits.each do |c|
-    break unless c
-    break if counter == 10
-    puts "  Date: #{c['commit']['author']['date']}"
-    puts "    #{c['commit']['message']}"
-    counter += 1
+  def initialize(token=ENV['GH'], per_page=10)
+    @github = Github.new(oauth_token: token, per_page: per_page)
+    @repos = @github.repos.list(user: 'yxlau', sort: 'updated')
   end
-  puts
+
+  def latest_repos
+    puts "Latest Repos:"
+    @repos.each do |r|
+      puts r.name.upcase
+      commits = @github.repos.commits.list('yxlau', r.name)
+      counter = 0
+      commits.each do |c|
+        break unless c
+        break if counter == 10
+        puts "  Date: #{c['commit']['author']['date']}"
+        puts "    #{c['commit']['message']}"
+        counter += 1
+      end
+      puts
+      sleep 1
+    end
+  end
+
 end
