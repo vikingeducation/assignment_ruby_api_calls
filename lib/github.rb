69ad30d3a24fff932b7032824c400b4c345519ae
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-01-30 18:01:13 +0800

    Add github

---
+++ lib/github.rb
@@ -0,0 +1,21 @@
+require 'github_api'
+
+github = Github.new(oauth_token: ENV['GH'], per_page: 10)
+
+repos = github.repos.list(user: 'yxlau', sort: 'updated')
+
+puts "Latest Repos:"
+repos.each do |r|
+  puts r.name.upcase
+  sleep 2
+  commits = github.repos.commits.list('yxlau', r.name)
+  counter = 0
+  commits.each do |c|
+    break unless c
+    break if counter == 10
+    puts "  Date: #{c['commit']['author']['date']}"
+    puts "    #{c['commit']['message']}".rjust(10)
+    counter += 1
+  end
+  puts
+end
