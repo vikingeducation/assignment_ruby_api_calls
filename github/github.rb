5f1a63a6ab0d68da97f0217dadc476d29044f505
Roy Chen <roychenlei@gmail.com>
2017-05-04 14:05:54 +0800

    added #most_recently_created_repos method

--- github/github.rb
+++ github/github.rb
@@ -9,9 +9,22 @@ class GithubAPIWrapper
   def initialize
     @github = Github.new(oauth_token: TOKEN)
   end
+
+  # returns the names of the ten most recently created public repos
+  def most_recently_created_repos(username = 'roychen25', num = 10)
+    repo_list = github.repos.list(user: username).sort_by { |repo| Time.parse(repo['created_at']) }.reverse[0..num - 1]
+
+    sleep 0.5
+
+    repo_names = []
+    repo_list.each { |repo| repo_names << repo['name'] }
+
+    repo_names
+  end
 end
 
 if $0 == __FILE__
   wrapper = GithubAPIWrapper.new
-  pp wrapper.github
+
+  pp wrapper.most_recently_created_repos
 end
