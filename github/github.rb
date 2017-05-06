b3147958f1bc7df1de3a38b68936f7ad05da9a4a
Roy Chen <roychenlei@gmail.com>
2017-05-06 11:36:44 +0800

    added #most_recently_updated_repos method

--- github/github.rb
+++ github/github.rb
@@ -21,10 +21,23 @@ class GithubAPIWrapper
 
     repo_names
   end
+
+  # returns the names of the ten most recently modified public repos
+  def most_recently_updated_repos(username = 'roychen25', num = 10)
+    repo_list = github.repos.list(user: username).sort_by { |repo| Time.parse(repo['updated_at']) }.reverse[0..num - 1]
+
+    sleep 0.5
+
+    repo_names = []
+    repo_list.each { |repo| repo_names << repo['name'] }
+
+    repo_names
+  end
 end
 
 if $0 == __FILE__
   wrapper = GithubAPIWrapper.new
 
-  pp wrapper.most_recently_created_repos
+  # pp wrapper.most_recently_created_repos
+  pp wrapper.most_recently_updated_repos
 end
