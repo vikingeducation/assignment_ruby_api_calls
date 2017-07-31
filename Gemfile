13b7f59a6b6fb19c3cb495e617fdff58b4529ea9
Sean Kelly <shonkelli@gmail.com>
2017-07-31 15:28:19 -0400

    Refactored and made API key live.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 gem 'rspec'
 gem 'guard-rspec'
 gem 'httparty'
-# gem 'pry-byebug'
--- lib/example.rb
+++ lib/example.rb
@@ -3,10 +3,17 @@ require 'date'
 require 'pry-byebug'
 w = WeatherForecast.new
 
-# w.hi_temps
-# w.lo_temps
+
+w.hi_temps
+
+w.lo_temps
+
 w.today
-# w.tomorrow
-# w.is_raining
-binding.pry
+
+w.tomorrow
+
+w.rain_forecast
+
 w.five_day
+
+w.basic_five_day
