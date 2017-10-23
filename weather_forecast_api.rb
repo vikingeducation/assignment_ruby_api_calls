945f972d247ebc81425cd06d55222edb921ba16c
anne <richardson.ae@gmail.com>
2017-10-23 09:51:19 -0500

    WIP high_temps method

--- weather_forecast_api.rb
+++ weather_forecast_api.rb
@@ -32,7 +32,32 @@ class WeatherForecast
     end
   end
 
+  def high_temps
+    # should be a collection of the high temperatures you get back, organized by date
+    retrieve_response
 
+    puts "Weather for #{@raw_response['city']['name']}"
+    @raw_response['list'].each do |date|
+
+      max = date['main']['temp_max']
+      # p convert_to_fahrenheit(max)
+
+      p "#{date['dt_txt']}: #{convert_to_fahrenheit(max)}"
+    end
+  end
+
+
+
+  private
+
+  def retrieve_response
+    @raw_response = JSON.parse(File.read("data/temp.json"))
+  end
+
+  def convert_to_fahrenheit(k)
+    f = (1.8 * (k - 273)) + 32
+    f.round(1)
+  end
 end
 
 forecast = WeatherForecast.new(location: 15601, days: 5)
