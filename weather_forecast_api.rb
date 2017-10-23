894d8d709fdf4d10ad479236250b85f1825c6ef1
anne <richardson.ae@gmail.com>
2017-10-23 13:34:20 -0500

    Chenge 'location' to 'zip' for clarity

--- weather_forecast_api.rb
+++ weather_forecast_api.rb
@@ -9,22 +9,18 @@ require 'date'
 require 'pry'
 
 class WeatherForecast
-  attr_accessor :raw_response
-  def initialize(location: 70115, days: 1)
-    @location = location
+
+  def initialize(zip: 70115, days: 1)
+    @zip = zip
     @days = days
     @raw_response = {}
   end
 
   API_KEY = ENV['API_KEY']
-
   BASE_URI = "http://api.openweathermap.org"
 
-  # url = "api.openweathermap.org/data/2.5/forecast/daily?q={city name},{country code}&cnt={cnt}"
-
-
   def send_request
-    url = "#{BASE_URI}/data/2.5/forecast?zip=#{@location}&APPID=#{API_KEY}"
+    url = "#{BASE_URI}/data/2.5/forecast?zip=#{@zip}&cnt=#{@days}&APPID=#{API_KEY}"
     response = HTTParty.get(url)
     save_response(response)
   end
