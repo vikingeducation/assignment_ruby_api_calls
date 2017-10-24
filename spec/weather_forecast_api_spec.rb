78275abaea5db9b911caa6e6f35b5f23dbe8cd57
anne <richardson.ae@gmail.com>
2017-10-24 09:00:54 -0500

    Write specs for #initilize

--- spec/weather_forecast_api_spec.rb
+++ spec/weather_forecast_api_spec.rb
@@ -1,5 +1,31 @@
 require 'weather_forecast_api'
 
-define WeatherForecast do
+describe WeatherForecast do
+
+  let(:forecast) { WeatherForecast.new(zip: 90210, days: 5) }
+
+  describe '#initialize' do
+    it("is a WeatherForecast") { expect(forecast).to be_a(WeatherForecast) }
+
+    f = WeatherForecast.new
+    it 'has default values for zip and days count' do
+      expect { WeatherForecast.new }.not_to raise_error
+    end
+  end
+
+  # describe '#send_request' do
+  # end
+
+  # describe '#high_temps' do
+  # end
+
+  # describe '#low_temps' do
+  # end
+
+  # describe '#today' do
+  # end
+
+  # describe '#tomorrow' do
+  # end
 
 end
